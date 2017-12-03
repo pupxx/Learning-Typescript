3827afda30ce66a9f45a74ef9d6cc1e5830b24d6
Cheryln <cherylnbarber@gmail.com>
2017-12-03 15:58:15 -0800

    Learning typescript. Installed typescript. Ran tsc init. Installed liteserver.

---
+++ app.js
@@ -0,0 +1,15 @@
+"use strict";
+console.log("it works");
+//  string
+var myName = "Cheryln";
+// myName = 20;  this gives us an error
+//number
+var myAge = 44;
+//myAge = 'Cheryln'  this gives us an error.
+//boolean
+var hasHobbies = false;
+// hasHobbies = 1;  this is an error even though 1 is true in JS.
+//assign types
+var myRealAge;
+myRealAge = 44;
+// myRealAge = "27";
---
+++ app.ts
@@ -0,0 +1,17 @@
+console.log("it works");
+//  string
+let myName = "Cheryln";
+// myName = 20;  this gives us an error
+
+//number
+let myAge = 44;
+//myAge = 'Cheryln'  this gives us an error.
+
+//boolean
+let hasHobbies = false;
+// hasHobbies = 1;  this is an error even though 1 is true in JS.
+
+//assign types
+let myRealAge: number;
+myRealAge = 44;
+// myRealAge = "27";
