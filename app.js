ba3fc8617bc41218ae5831beda4d845e003e7de6
Cheryln <cherylnbarber@gmail.com>
2017-12-03 19:16:16 -0800

    Added examples of different types.

--- app.js
+++ app.js
@@ -13,3 +13,51 @@ var hasHobbies = false;
 var myRealAge;
 myRealAge = 44;
 // myRealAge = "27";
+// array
+var hobbies = ["Cooking", "Sports"];
+hobbies = [100]; // this is ok since hobbies is type any.
+//  tuples has to follow the layout.  ie) 2 items and first has to be a string and second has to be a number.
+var address = ["Sputersreet", 99];
+// Enums
+var Color;
+(function (Color) {
+    Color[Color["Gray"] = 0] = "Gray";
+    Color[Color["Green"] = 100] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+var myColor = Color.Blue;
+console.log(myColor);
+// the assigned type refers to the return value.
+function returnMyName() {
+    return myName;
+}
+console.log(returnMyName());
+//void -- this function does not return anything.  If I try to return anything it will error since I told it void.
+function sayHello() {
+    console.log("Hello");
+}
+// argument types.
+function multiply(value1, value2) {
+    return value1 * value2;
+}
+console.log(multiply(2, 3));
+// function as types
+// function are types on their own.
+// I can define the format that the function must be for this variable to accept it.
+var myMultiply;
+// myMultiply = sayHello;
+// myMultiply();
+myMultiply = multiply;
+console.log(myMultiply(2, 3));
+// objects
+var userData = {
+    name: 'Cheryln',
+    age: 27
+};
+// complex object
+var complex = {
+    data: [1, 2, 3],
+    output: function (all) {
+        return this.data;
+    }
+};
--- app.ts
+++ app.ts
@@ -15,3 +15,59 @@ let hasHobbies = false;
 let myRealAge: number;
 myRealAge = 44;
 // myRealAge = "27";
+
+// array
+let hobbies: any[] = ["Cooking", "Sports"]
+hobbies=[100] // this is ok since hobbies is type any.
+
+//  tuples has to follow the layout.  ie) 2 items and first has to be a string and second has to be a number.
+let address: [string, number] = ["Sputersreet", 99];
+
+// Enums
+enum Color {
+    Gray,
+    Green = 100,
+    Blue = 2
+}
+let myColor: Color = Color.Blue;
+console.log(myColor)
+
+// the assigned type refers to the return value.
+function returnMyName(): string{
+    return myName
+}
+console.log(returnMyName())
+
+//void -- this function does not return anything.  If I try to return anything it will error since I told it void.
+function sayHello(): void{
+    console.log("Hello")
+}
+
+// argument types.
+function multiply(value1: number, value2: number): number {
+    return value1 * value2
+}
+console.log(multiply(2, 3))
+
+// function as types
+// function are types on their own.
+// I can define the format that the function must be for this variable to accept it.
+let myMultiply: (val1: number, val2: number)=> number;
+// myMultiply = sayHello;
+// myMultiply();
+myMultiply = multiply;
+console.log(myMultiply(2,3))
+
+// objects
+let userData: {name: string, age: number} = {
+    name: 'Cheryln', 
+    age: 27
+}
+
+// complex object
+let complex: {data: number[], output: (all: boolean)=>number[]} = {
+    data: [1,2,3], 
+    output: function (all: boolean): number[] {
+        return this.data;
+    }
+}
